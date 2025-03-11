a6f55bcf552be43d9c1da0c038f5b6d8d3eee84d
Jon Whittlestone <jwhittlestone@hotmail.com>
2025-03-11 12:27:21 +0000

    styling

---
+++ src/components/GalleryItem.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+interface GalleryItemProps {
+  imageSrc: string;
+  altText: string;
+}
+
+export default function GalleryItem({ imageSrc, altText }: GalleryItemProps) {
+  return (
+    <div className="gallery-item">
+      <Image
+        src={imageSrc}
+        alt={altText}
+        width={400}
+        height={300}
+        className="gallery-image"
+      />
+      <p className="gallery-caption">{altText}</p>
+    </div>
+  );
+}
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,28 +1,17 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Logo() {
   return (
     <div className="logo">
       <Link href="/">
-        <svg
-          width="200"
-          height="200"
-          viewBox="0 0 200 200"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="100" cy="100" r="90" fill="#F8DED8" stroke="none" />
-          <text
-            x="50%"
-            y="50%"
-            fontFamily="Baskerville"
-            fontSize="82.5"
-            fill="#800020"
-            textAnchor="middle"
-            dominantBaseline="middle"
-          >
-            allo
-          </text>
-        </svg>
+        <Image
+          src="/images/nav-gr-logo.png"
+          alt="Allo Interiors Logo"
+          width={200}
+          height={200}
+          priority
+        />
       </Link>
     </div>
   );
